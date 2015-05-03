e41a4cc471032d28e4286dec6eba7880608b90b5
Tara Jean Konradi <tarakonradi@gmail.com>
2015-05-03 15:58:58 -0400

    homework

---
+++ L6JQuerywebpage.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+$(".header").animate({
+	width: 300
+}, 3000);
+
+$(".oink").click(function() {
+	alert ('oink!'); 
+})
+
+$(".pic").fadeIn(5000)
+
+$(".two, .one").slideDown(2000);
+
+})
